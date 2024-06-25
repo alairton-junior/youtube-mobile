2886bc532133b89149d6126c1fda38c6acdcc51b
Sadn0x <sidneybragacf@outlook.com>
2024-06-25 20:32:45 -0300

    Ajustes na responsividade do 404

--- src/Components/Header/Logo.jsx
+++ src/Components/Header/Logo.jsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom'
 import YtbLogo from '../../assets/imgs/logos/youtube_logo.svg'
 
 export function Logo () {
     return (
-        <img src={YtbLogo} alt="Logo Youtube" />
+        <Link to="/">
+            <img src={YtbLogo} alt="Logo Youtube" />
+        </Link>
     )
 }
--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -13,8 +13,8 @@ export function Page404() {
         </div>
         <div className="p404-text">
             <h1>Error 404</h1>
-            <h4>Our developers are creating that page yet, come back later<br/>
-             or click <Link to="/"><button>HERE</button></Link> to go back Home.</h4>
+            <h4>Our developers are creating that page yet, come back later
+             or <Link to="/"><button>CLICK HERE</button></Link> to go back Home.</h4>
         </div>
                 
         </div>
