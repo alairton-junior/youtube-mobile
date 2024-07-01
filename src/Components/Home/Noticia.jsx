27130ec95baf6391acf78acb05135a4c20fa680d
Esthefan Castro <raelye8@gmail.com>
2024-07-01 18:54:42 -0300

    Add Noticia component and update Home page assets
    - Added Noticia component in src/Components/Home/Noticia.jsx
    - Added new avatar image in src/assets/imgs/avatarlogo2.png
    - Added UX image in src/assets/imgs/thumbnails/UX.png
    - Updated Home page in src/pages/Home/Index.jsx to include Noticia component and new assets

---
+++ src/Components/Home/Noticia.jsx
@@ -0,0 +1,21 @@
+import MoreIcon from '../../assets/imgs/icons/More.svg'
+import {Avatar} from '../Root/Avatar'
+
+
+export function Noticias ({image, title, description, avatarImage}) {
+    return (
+        <>
+            <img src={image} alt={title} width="100%" style={{marginTop: 10, width: 'unset', marginLeft: -20}} />
+            <div style={{display: 'flex', gap: 10, alignItems: 'center', marginTop: '-10px'}}>
+                <div style={{display: 'flex', alignItems: 'center', gap: 10}}>
+                    <Avatar avatarImage={avatarImage}/>
+                    <h2>{title}</h2>
+                </div>
+                <div>
+                    <img src={MoreIcon} style={{width: 50, height: 50, position: 'relative', right: '-10px', top: 10}}/>
+                </div>
+            </div>
+            <h4 className="description" style={{marginTop: -5, position: 'relative', left: 47}}>{description}</h4>
+        </>
+    )
+}
--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,9 +1,12 @@
 import { Explorer } from "../../Components/Home/Explorer";
 import { Video } from "../../Components/Home/Video";
+import {Noticias} from "../../Components/Home/Noticia.jsx";
 
 import Thumbinails1 from '../../assets/imgs/thumbnails/video1.jpg'
-import Thumbinails2 from '../../assets/imgs/thumbnails/noticia1.jpg'
+import Thumbinails2 from '../../assets/imgs/thumbnails/noticia1.png'
+import Thumbinails3 from '../../assets/imgs/thumbnails/UX.png'
 import Avatar1 from '../../assets/imgs/Avatar2.jpg'
+
 
 export function Index () {
     return (
@@ -15,7 +18,7 @@ export function Index () {
             title="Breaking! NASA discovers alien life on Mars " 
             description="NASA • 99.2M views • Now"/>
         
-        <Noticias //Esthefan
+        <Noticias //Esthefann
             avatarImage={Avatar1}
             image={Thumbinails2} 
             title="1 year traveling with my van in Europe! " 
@@ -23,15 +26,16 @@ export function Index () {
 
         <Video 
             avatarImage={Avatar1}
-            image={Thumbinails1} 
-            title="Breaking! NASA discovers alien life on Mars " 
-            description="NASA • 99.2M views • Now"/>
+            image={Thumbinails3} 
+            title="Volcano goes boom " 
+            description="BoomChanne• 82k views • 6 months ago"/>
 
         <Video 
             avatarImage={Avatar1}
             image={Thumbinails1} 
-            title="Breaking! NASA discovers alien life on Mars " 
-            description="NASA • 99.2M views • Now"/>
+            title="DesignER-rrors " 
+            description="UxDesign • 120k views • 2 months ago"/>
         </>
+        
     )
 }
